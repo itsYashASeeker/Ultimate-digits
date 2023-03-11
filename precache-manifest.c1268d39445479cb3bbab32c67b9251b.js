6f08c63df15c601b66dc40b91c5ed645d9c1a4b9
itsYashASeeker <yashkc6432@gmail.com>
2023-03-11 15:24:36 +0530

    Updates

---
+++ precache-manifest.c1268d39445479cb3bbab32c67b9251b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc955195053b5199a4e8",
+    "url": "/Ultimate-digits/static/css/main.7c2200f1.chunk.css"
+  },
+  {
+    "revision": "bc955195053b5199a4e8",
+    "url": "/Ultimate-digits/static/js/main.98aaabaf.chunk.js"
+  },
+  {
+    "revision": "7bcec787f17800c866ca",
+    "url": "/Ultimate-digits/static/js/runtime~main.e13b90d8.js"
+  },
+  {
+    "revision": "a12096f080c935b4baa4",
+    "url": "/Ultimate-digits/static/js/2.879d6def.chunk.js"
+  },
+  {
+    "revision": "9f5fd5f3c476bb86b433",
+    "url": "/Ultimate-digits/static/js/3.6ccac940.chunk.js"
+  },
+  {
+    "revision": "7c627bfc9dd4ef2dc57ef7f92fc01486",
+    "url": "/Ultimate-digits/static/media/iphone.7c627bfc.png"
+  },
+  {
+    "revision": "c43595100681175f5855fc6ad02b54a1",
+    "url": "/Ultimate-digits/static/media/lower_arc.c4359510.png"
+  },
+  {
+    "revision": "01842e0b7d724c0e1799d47ee568d6dd",
+    "url": "/Ultimate-digits/index.html"
+  }
+];
